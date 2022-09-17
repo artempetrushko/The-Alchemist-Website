bbbc9cb0bed7e94e71387e8dbf76437031349012
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2022-09-17 16:50:26 +0500

    commit 3

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+const formState = {
+    Shown : 'block',
+    Hidden : 'none'
+};
+
+var authorizationSection = document.getElementById("authorization-section");
+var authorizationForm = document.getElementById('authorization-form')
+var isMouseOverForm = false;
+
+authorizationForm.addEventListener('mouseenter', function(e) {
+    isMouseOverForm = true;
+});
+authorizationForm.addEventListener('mouseleave', function(e) {
+    isMouseOverForm = false;
+});
+
 function showSubsection(subsectionNumber) {
     gameSubDescriptions = document.getElementsByClassName("subsection-description");
     for (let i = 1; i <= 3; i++) {
         gameSubDescriptions[i - 1].style.display = i === subsectionNumber ? 'table' : 'none';
+    }
+}
+
+function showAuthorizationForm() {
+    toggleAuthorizationForm(formState.Shown);
+}
+
+function hideAuthorizationForm() {
+    toggleAuthorizationForm(formState.Hidden);
+}
+
+function toggleAuthorizationForm(state) {
+    if (!isMouseOverForm) {
+        authorizationSection.style.display = state;
     }
 }
