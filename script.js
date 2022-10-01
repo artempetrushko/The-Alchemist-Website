2339aa8045e268362a485e77a1472382ec3e164d
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2022-10-01 22:07:29 +0500

    Made gallery worked

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 var authorizationSection = $('.authorization-section');
 var authorizationForm = $('.authorization-form');
 var registrationForm = $('.registration-form');
+var feedbackSendForm = $('.feedback-send-form');
+var galleryCarousel = $('.gallery-carousel');
 var lastOpenedForm = null;
 
+var selectedPhotoNumber = 1;
+var galleryCarousel_LeftBorder = 1;
+var galleryCarousel_RightBorder = 4;
+var galleryPhotosCount = 7;
+
 $('.main-photo').attr("src", "resources/gallery-photo-1.png");
-$('.additional-photo.photo-1').css('border', '2px solid white')
-                              .css('transform', 'scale(1.1)');
+changeGalleryPhotoStyle(1, true);
+setArrowButtonVisibility();
 
 $('.user-info').hide();
 $('.feedback-form').hide();
 
 showSubsection(1);
 
-$('.additional-photo').click(function() {
-    var imageSource = $(this).attr("src");
-    $('.main-photo').attr("src", imageSource);
+for (var i = 5; i <= galleryPhotosCount; i++) {
+    $('<img class="gallery-photo additional-photo"></img>').addClass('photo-' + i)
+                                                           .attr('src', 'resources/gallery-photo-' + i +  '.png')
+                                                           .appendTo(galleryCarousel)
+                                                           .hide();
+}
 
-    for (var i = 1; i <= 4; i++) {
-        var currentPhoto = $('.additional-photo.photo-' + i);      
-        currentPhoto.css('border', currentPhoto.is($(this)) ? '2px solid white' : 'none');
-        currentPhoto.css('transform', currentPhoto.is($(this)) ? 'scale(1.1)' : 'none');
+$('.additional-photo').click(function() {
+    $('.main-photo').attr("src", $(this).attr("src"));
+    
+    changeGalleryPhotoStyle(selectedPhotoNumber, false);
+    for (var i = galleryCarousel_LeftBorder; i <= galleryCarousel_RightBorder; i++) {
+        if ($('.additional-photo.photo-' + i).is($(this))) {
+            selectedPhotoNumber = i;
+            changeGalleryPhotoStyle(selectedPhotoNumber, true);
+        }
     }
+    setArrowButtonVisibility();
 });
+
+function changeGalleryMainPhoto(offset) {
+    if (selectedPhotoNumber + offset < galleryCarousel_LeftBorder || selectedPhotoNumber + offset > galleryCarousel_RightBorder) {
+        galleryCarousel.children().eq((selectedPhotoNumber - 1) - 3 * offset).hide();
+        galleryCarousel.children().eq((selectedPhotoNumber - 1) + 1 * offset).show();
+        galleryCarousel_LeftBorder += offset;
+        galleryCarousel_RightBorder += offset;
+    }
+        
+    changeGalleryPhotoStyle(selectedPhotoNumber, false);
+    selectedPhotoNumber += offset;
+    changeGalleryPhotoStyle(selectedPhotoNumber, true);
+    $('.main-photo').attr("src", "resources/gallery-photo-" + selectedPhotoNumber + ".png");
+    setArrowButtonVisibility();
+}
+
+function changeGalleryPhotoStyle(photoNumber, isSelected) {
+    var currentPhoto = $('.additional-photo.photo-' + photoNumber);      
+    currentPhoto.css('border',    isSelected ? '2px solid white' : 'none');
+    currentPhoto.css('transform', isSelected ? 'scale(1.1)'      : 'none');
+}
+
+function setArrowButtonVisibility() {
+    $('.gallery-arrow-left').css('visibility', selectedPhotoNumber === 1 ? 'hidden' : 'visible');
+    $('.gallery-arrow-right').css('visibility', selectedPhotoNumber === galleryPhotosCount ? 'hidden' : 'visible');
+}
 
 function showSubsection(subsectionNumber) {
     gameSubDescriptions = $('.subsection-description');
